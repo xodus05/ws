c71bd12ca85891d6ad4b4a7f7b85499debd671b8
xodus05 <s2121@e-mirim.hs.kr>
2022-11-10 12:47:46 +0900

    데이터 학습

--- script.js
+++ script.js
@@ -69,6 +69,29 @@ function convertToTensor(data) {
     });
 }
 
+async function trainModel(model, inputs, labels) {
+    // Prepare the model for training.
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+    });
+
+    const batchSize = 32;
+    const epochs = 50;
+
+    return await model.fit(inputs, labels, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+            { name: 'Training Performance' },
+            ['loss', 'mse'],
+            { height: 200, callbacks: ['onEpochEnd'] }
+        )
+    });
+}
+
 async function run() {
     // Load and plot the original input data that we are going to train on.
     const data = await getData();
@@ -91,6 +114,14 @@ async function run() {
     // Create the model
     const model = createModel();
     tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+    // Convert the data to a form we can use for training.
+    const tensorData = convertToTensor(data);
+    const {inputs, labels} = tensorData;
+
+    // Train the model
+    await trainModel(model, inputs, labels);
+    console.log('Done Training');
 }
 
 document.addEventListener('DOMContentLoaded', run);
